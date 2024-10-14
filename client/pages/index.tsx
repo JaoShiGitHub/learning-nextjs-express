7c6d0ee2aa9d2dd5f9d00d77706a5579d7424caf
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-14 16:52:29 +0700

    display the data

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,18 +2,20 @@ import React, { useState, useEffect } from "react";
 
 function index() {
 
+  const [message, setMessage] = useState("Loading")
+
   useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
       response => response.json()
     ).then(
       data => {
         console.log(data);
-
+        setMessage(data.message)
       }
     )
   }, [])
 
-  return <div>index</div>;
+  return <div>{message}</div>;
 }
 
 export default index;
