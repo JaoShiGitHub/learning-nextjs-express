7707f1d5f29cb0ec74699a8e9da7e7fbb4ceb279
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-10-14 17:15:57 +0700

    Try fetching array data

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 function index() {
 
   const [message, setMessage] = useState("Loading")
+  const [people, setPeople] = useState([])
 
   useEffect(() => {
     fetch("http://localhost:8080/api/home").then(
@@ -11,11 +12,22 @@ function index() {
       data => {
         console.log(data);
         setMessage(data.message)
+        setPeople(data.people)
       }
     )
   }, [])
 
-  return <div>{message}</div>;
+  return (
+    <div>
+      <div>{message}</div>
+      {people.map((person, index) => (
+        <div key={index}>
+          {person}
+        </div>
+      ))
+      }
+    </div>
+  );
 }
 
 export default index;
